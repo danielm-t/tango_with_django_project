038b00b2c28e24b3c7a0e98e5e64406ba77d7acf
“Mihai-Daniel <2569281t@student.gla.ac.uk>
2022-01-29 19:15:37 +0000

    Chapter 7 finished and tests completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib import admin
 from rango.models import Category, Page
 
 
--- rango/forms.py
+++ rango/forms.py
@@ -1,8 +1,3 @@
-from dataclasses import fields
-from pyexpat import model
-import re
-from socket import fromshare
-from statistics import mode
 from django import forms
 from rango.models import Page, Category
 
@@ -19,11 +14,19 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    name = forms.CharField(
-        max_length=128, help_text="Please enter the category name.")
+    title = forms.CharField(
+        max_length=128, help_text="Please enter the title of the page.")
     url=forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     
     class Meta:
         model=Page
         exclude=('category',)
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
