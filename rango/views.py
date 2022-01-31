4e8fc5f07e79268e8aab143a0897c1c8382df1e5
“Mihai-Daniel <2569281t@student.gla.ac.uk>
2022-01-31 00:32:33 +0000

    Finished second part of Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -24,9 +24,9 @@ def about(request):
 
 
 def about(request):
-    context_dict = {
-        'boldmessage': 'This tutorial has been put together by Daniel Tudose'}
-    return render(request, 'rango/about.html', context=context_dict)
+   print(request.method)
+   print(request.user)
+   return render(request, 'rango/about.html', {})
 
 
 def show_category(request, category_name_slug):
