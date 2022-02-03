dd0a0e15e4b161d04ec03e178cdffe3e6b25971c
“Mihai-Daniel <2569281t@student.gla.ac.uk>
2022-02-03 01:46:11 +0000

    Chapter 9 finished with 2 errors

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
 
 
 class CategoryForm(forms.ModelForm):
@@ -13,15 +15,18 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ('name',)
 
+
 class PageForm(forms.ModelForm):
     title = forms.CharField(
         max_length=128, help_text="Please enter the title of the page.")
-    url=forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    url = forms.URLField(
+        max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    
+
     class Meta:
-        model=Page
-        exclude=('category',)
+        model = Page
+        exclude = ('category',)
+
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
@@ -30,3 +35,17 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
